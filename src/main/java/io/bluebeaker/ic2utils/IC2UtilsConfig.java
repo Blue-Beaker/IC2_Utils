cc3ed004f346d454383e46697ded8297bfa81e2f
blue-beaker <yyy5566778899@126.com>
2025-07-05 15:45:11 +0800

    Remove excess config and lang entries for crop patch

--- src/main/java/io/bluebeaker/ic2utils/IC2UtilsConfig.java
+++ src/main/java/io/bluebeaker/ic2utils/IC2UtilsConfig.java
@@ -7,10 +7,6 @@ import net.minecraftforge.common.config.Config.Type;
 
 @Config(modid = IC2UtilsMod.MODID,type = Type.INSTANCE,category = "general")
 public class IC2UtilsConfig {
-    @Comment("Prevent breaking crop sticks when walking over it")
-    @LangKey("config.ic2utils.noTrampleCrops.name")
-    public static boolean noTrampleCrops = true;
-
     @Comment("Disable to remove default recipes of Electric Wrench (New).")
     @LangKey("config.ic2utils.wrenchRecipes.name")
     @Config.RequiresMcRestart
